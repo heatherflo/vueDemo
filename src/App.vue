bd8d546a83b3aed85dd02e3af6d01c47a778cf1f
Heather Floyd <heatherflo@gmail.com>
2024-01-04 10:46:15 -0700

    saving vueDemo

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="teams">
+    <div v-for="teamData in state.teams" :key="teamData.id">
+      <!-- Step 3:inserting the team component into the HTML where you want it. Note self-closing tag. Must add class binder for the child component to recognize it. SO>> :prop being passed down=objectData/value being passed down. It is getting passed as an attribute on a component tag  -->
+      <team :teamProp="teamData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+// step one: import the Team component 
+import Team from './components/Team.vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      teams: [{
+        id: "1a",
+        name: "Red Team",
+        score: 0,
+      }, {
+        id: "2a",
+        name: "Blue Team",
+        score: 0,
+      }, {
+        id: "3a",
+        name: "Yellow Team",
+        score: 0,
+      }, {
+        id: "4a",
+        name: "Green Team",
+        score: 0,
+      }]
+    })
+    return {
+      state
+    }
+  },
+  // step two: add component you want injected outside of the setup
+  components: {
+    Team
+  }
+}
+</script>
+<style >
+.teams {
+  display: flex;
+  justify-content: center;
+  margin: 8px
+}
+</style>
---
+++ src/components/Team.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="team">
+    <h1 v-if="teamProp.score > 9" :class="{ green: teamProp.score > 9 }">{{ teamProp.name }}</h1>
+    <h1 v-else="teamProp.score > 9" :class="{ red: teamProp.score <= 9 }">{{ teamProp.name }}</h1>
+    <h3>Score: {{ teamProp.score }}</h3>
+    <button :disabled="teamProp.score > 9" @click="addPoint()">+{{ teamProp.score }}</button>
+    <h3 v-if="teamProp.score > 9">You're the winner!</h3>
+    <h3 v-else="teamProp.score">You're a loser!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  //step four: make a props value with the name of the prop you are passing down in the app.vue
+  props: ['teamProp'], //the data we are passing down from the parent to the child are inside an array - the prop name we have given it. App.vue is our parent and Team.vue is our child. We are EXPORTING the prop into the parent - as the child component so it can read it 
+  setup(props) {
+
+    return {
+      addPoint() {
+        props.teamProp.score++
+        //pulls the property teamProp off the props value and accesses the score
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.team {
+  justify-content: center;
+  margin: 1em
+}
+
+.green {
+  color: green
+}
+
+.red {
+  color: red
+}
+</style>
